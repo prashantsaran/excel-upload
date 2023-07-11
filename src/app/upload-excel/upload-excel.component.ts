e095b862b1ffc5c3288201d360ba4eb237aad5b6
Prashant <prashantsaransingh@gmail.com>
2023-07-11 12:11:59 +0530

    Add upload-excel.component.ts

---
+++ src/app/upload-excel/upload-excel.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { DetailsServiceService } from '../service/details-service.service';
+
+@Component({
+  selector: 'app-upload-excel',
+  templateUrl: './upload-excel.component.html',
+  styleUrls: ['./upload-excel.component.css']
+})
+export class UploadExcelComponent {
+  exceldata: any
+  excelData: any
+  constructor(private detailService: DetailsServiceService) { }
+
+  // Function to read the uploaded Excel file
+  readExcel(event: any) {
+
+    const file = event.target.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e) => {
+
+      // Reading binary data from the Excel sheet
+      const workBook = XLSX.read(fileReader.result, { type: 'binary' });
+      const sheetNames = workBook.SheetNames;
+
+      //converting sheetdata to json
+      this.exceldata = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
+
+
+
+
+      this.detailService.addDetails(this.exceldata);
+      this.detailService.postDetails.subscribe(response => {
+
+        if (response.status === 200) {
+          console.log('&&', "Data has been added successfully");
+        }
+      });
+    };
+  }
+}
